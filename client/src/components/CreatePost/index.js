ed3489df37318844ed5163d4d1c1e2ff66674800
Chaz <ctol1994@gmail.com>
2019-08-12 21:00:00 -0700

    changed entry addition to a modal

--- client/src/components/CreatePost/index.js
+++ client/src/components/CreatePost/index.js
@@ -1,5 +1,13 @@
-import React from "react";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import React, { useState } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Modal,
+  ModalBody
+} from "reactstrap";
 import { useDispatch } from "react-redux";
 import { addPost } from "../../redux/actions";
 import { useInput } from "../hooks";
@@ -10,7 +18,16 @@ const CreatePost = () => {
   const { value: content, bind: bindContent, reset: resetContent } = useInput(
     ""
   );
+  const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
+
+  const toggle = () => {
+    setOpen(!open);
+    if (!open) {
+      resetTitle();
+      resetContent();
+    }
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -20,46 +37,73 @@ const CreatePost = () => {
         content
       })
     );
+    // coming back to this later to only reset on success
     resetTitle();
     resetContent();
   };
 
   return (
-    <div className="form">
-      <Form onSubmit={handleSubmit}>
-        <FormGroup>
-          <Label for="title">
-            <b>Title</b>
-          </Label>
-          <Input
-            type="text"
-            name="title"
-            id="title"
-            bsSize="lg"
-            placeholder="Type your Title..."
-            {...bindTitle}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="content">
-            <b>Content</b>
-          </Label>
-          <Input
-            type="textarea"
-            name="text"
-            id="content"
-            placeholder="Type your content..."
-            bsSize="lg"
-            {...bindContent}
-            style={{ height: "200px" }}
-            required
-          />
-        </FormGroup>
-        <Button type="submit" value="Submit" color="dark">
-          Submit
-        </Button>
-      </Form>
+    <div className="post-edit">
+      <Button className="form-button" color="dark" onClick={toggle}>
+        <span className="plus" role="img" aria-label="plus">
+          &#43;
+        </span>{" "}
+        Journal Entry
+      </Button>
+      <Modal isOpen={open}>
+        <ModalBody>
+          <div className="form">
+            <Form onSubmit={handleSubmit}>
+              <FormGroup>
+                <Label for="title">
+                  <b>Title</b>
+                </Label>
+                <Input
+                  type="text"
+                  name="title"
+                  id="title"
+                  bsSize="lg"
+                  placeholder="Type your Title..."
+                  {...bindTitle}
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="content">
+                  <b>Content</b>
+                </Label>
+                <Input
+                  type="textarea"
+                  name="text"
+                  id="content"
+                  placeholder="Type your content..."
+                  bsSize="lg"
+                  {...bindContent}
+                  style={{ height: "200px" }}
+                  required
+                />
+              </FormGroup>
+              <Button
+                className="form-button"
+                type="submit"
+                value="Submit"
+                color="dark"
+                onClick={toggle}
+              >
+                Submit
+              </Button>
+              <Button
+                className="form-button"
+                type="button"
+                color="dark"
+                onClick={toggle}
+              >
+                Cancel
+              </Button>
+            </Form>
+          </div>
+        </ModalBody>
+      </Modal>
     </div>
   );
 };
