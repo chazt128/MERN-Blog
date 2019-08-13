7692b41d87a60afed64409a778ffa795c3b4a1d5
Chaz <ctol1994@gmail.com>
2019-08-13 00:17:01 -0700

    update post saga

---
+++ client/src/redux/sagas/UpdatePostSaga.js
@@ -0,0 +1,21 @@
+import { take, fork, call, put } from "redux-saga/effects";
+import { POSTS } from "../../constants";
+import { updatePost } from "../../api";
+import { loadPosts, setError } from "../actions";
+
+function* handleUpdatePost(post) {
+  try {
+    console.log("handling update", post);
+    yield call(updatePost, post);
+    yield put(loadPosts());
+  } catch (e) {
+    yield put(setError(e.toString()));
+  }
+}
+
+export default function* watchUpdatePost() {
+  while (true) {
+    const { payload } = yield take(POSTS.UPDATE_POST);
+    yield fork(handleUpdatePost, payload.post);
+  }
+}
