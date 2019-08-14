77347203cf96dd0fe542f9a67a87279e70a66668
Chaz <ctol1994@gmail.com>
2019-08-13 23:07:42 -0700

    refactored display

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -21,18 +21,21 @@ const PostList = () => {
   const error = useSelector(state => state.error);
   const message = useSelector(state => state.message);
   const loading = useSelector(state => state.loading);
+  const [initialLoad, setInitialLoad] = useState(false);
   const [filter, setFilter] = useState("");
-  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [postList, setPostList] = useState([]);
+
+  if (posts.length > 0 && postList.length === 0 && !initialLoad) {
+    console.log("POSTS", posts);
+    setPostList([...posts]);
+    setInitialLoad(true);
+  }
 
   useEffect(() => {
+    console.log("mount");
     dispatch(loadPosts());
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // useEffect(() => {
-  //   filterPosts(filter);
-  //       // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [])
+  }, [message]);
 
   const loadingCard = () => (
     <Card className="card">
@@ -70,16 +73,17 @@ const PostList = () => {
   );
 
   const filterPosts = text => {
-    const currentlyFiltered = posts.filter(post => {
-      const postDetails = `${post.title} ${post.content}`;
-      return postDetails.toLowerCase().includes(text.toLowerCase());
-    });
     setFilter(text);
-    setFilteredPosts(() => currentlyFiltered);
+    setPostList(
+      posts.filter(post => {
+        const postDetails = `${post.title} ${post.content}`;
+        return postDetails.toLowerCase().includes(text.toLowerCase());
+      })
+    );
   };
 
-  const displayFilteredPosts = () =>
-    filteredPosts.map(post => {
+  const displayPosts = () =>
+    postList.map(post => {
       const { _id, title } = post;
       let { date, content } = post;
       date = new Date(date).toDateString();
@@ -104,38 +108,8 @@ const PostList = () => {
       );
     });
 
-  const displayAllPosts = () =>
-    posts.map(post => {
-      const { _id, title } = post;
-      let { date, content } = post;
-      date = new Date(date).toDateString();
+  let noPosts = postList.length === 0;
 
-      return (
-        <Card className="card" key={_id}>
-          <CardBody>
-            <CardTitle>
-              <h3>{title}</h3>
-            </CardTitle>
-            <CardSubtitle>
-              <small>{date}</small>
-            </CardSubtitle>
-            <br />
-            <CardText className="card-text">{content}</CardText>
-            <div className="card-edit">
-              <EditPost className="post-btn" buttonLabel="Edit" post={post} />
-              <DeletePost className="post-btn" id={_id} />
-            </div>
-          </CardBody>
-        </Card>
-      );
-    });
-
-  let noPostsToShow =
-    !loading &&
-    (posts.length === 0 || (filter !== "" && filteredPosts.length === 0));
-  let showAll = posts.length > 0 && filter === "";
-  let showFiltered = filteredPosts.length > 0 && filter !== "";
-  console.log("FILTERED", filteredPosts);
   return (
     <div className="post-list">
       <Input
@@ -146,13 +120,12 @@ const PostList = () => {
         placeholder="Search for post..."
         value={filter}
         onChange={e => filterPosts(e.target.value)}
-      ></Input>
+      />
       {message && <Alert color="success">{message}</Alert>}
       {error && <Alert color="danger">{error}</Alert>}
       {loading && loadingCard()}
-      {noPostsToShow && noPostsCard()}
-      {showAll && displayAllPosts()}
-      {showFiltered && displayFilteredPosts()}
+      {noPosts && noPostsCard()}
+      {!noPosts && displayPosts()}
     </div>
   );
 };
